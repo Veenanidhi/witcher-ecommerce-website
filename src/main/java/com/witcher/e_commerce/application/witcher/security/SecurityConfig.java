e1253955d1fb5073c7cd1ee2a4384faff73a63ae
Veena <veenaasjune22@gmail.com>
2024-07-12 11:03:32 +0530

    message

--- src/main/java/com/witcher/e_commerce/application/witcher/security/SecurityConfig.java
+++ src/main/java/com/witcher/e_commerce/application/witcher/security/SecurityConfig.java
@@ -9,6 +9,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -33,6 +39,17 @@ public class SecurityConfig {
         return (web) ->
                 web.ignoring().requestMatchers("/static/css/img/**","/style/**","/uploads/**","/templates");
     }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:8080"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/", configuration);
+        return source;
+    }
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
